cfef5e58997ce7192270131ca0d6707982ed9f87
Buddika13 <140634145+Buddika13@users.noreply.github.com>
2024-04-21 17:05:48 +0530

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int no1,no2,no3,max;
+printf("Enter three numbers");
+scanf("%d,%d,%d",&no1,&no2,&no3);
+max=no1;
+if(no2>max);
+max=no2;
+if(no3>max);
+max=no3;
+printf("The highest number is %d\n",max);
+}
