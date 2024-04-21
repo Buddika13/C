6807e260efbfd4774c064ce438b81573228c0160
Buddika13 <140634145+Buddika13@users.noreply.github.com>
2024-04-21 17:05:09 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int no1,no2,total;
+printf("Enter first number\n");
+scanf("%d",&no1);
+printf("Enter second number\n");
+scanf("%d",&no2);
+total=no1+no2;
+printf("The total is %d\n",total);
+}
