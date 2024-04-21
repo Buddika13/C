b4692867d992e4eff5d0a528ec433eb7ebabdc48
Buddika13 <140634145+Buddika13@users.noreply.github.com>
2024-04-21 17:10:17 +0530

    Add files via upload

---
+++ 19.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int s,i;
+printf("Enter array size");
+scanf("%d",&s);
+int u[s],v[s],w[s];
+int sp=0;
+for(i=0;i<s;i++)
+{
+printf("Enter value to the element %d of u",i+1);
+scanf("%d",&u[i]);
+printf("Enter value to the element %d of v",i+1);
+scanf("%d",&v[i]);
+}
+int su=0,sv=0;
+for(i=0;i<s;i++)
+{
+sp=sp+w[i];
+printf("%d\n",w[i]);
+}
+printf("The scaler product is %d \n",sp);
+}
+
+
